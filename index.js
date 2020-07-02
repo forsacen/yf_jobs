595dc0a62ca067c0aed35ce85b9b8714446974a6
forsacen <forsacen@protonmail.com>
2020-07-03 04:19:09 +0800

    update

--- index.js
+++ index.js
@@ -6,29 +6,28 @@ function jobs(opt){
     this.count=0
     this.opt=opt
     this.done=this._done.bind(this)
+    this.on('schedule',async ()=>{
+        if(this.pool.length>0 && ((this.opt.limit>0 && this.count<this.opt.limit)||!this.opt.limit)){
+            this.count++
+            let data=this.pool.shift()
+            if(this.opt.callback&&typeof this.opt.callback=='function'){
+                this.opt.callback(data,this.done)
+            }else{
+                this.done()
+            }
+        }
+    })
 }
 
 util.inherits(jobs,EventEmitter)
 
 jobs.prototype.queue=function(data){
     this.pool.push(data)
-    this._schedule()
+    this.emit('schedule')
 }
 
 jobs.prototype.queueSize=function(){
     return this.pool.length
-}
-
-jobs.prototype._schedule=function(){
-    if(this.pool.length>0 && ((this.opt.limit>0 && this.count<this.opt.limit)||!this.opt.limit)){
-        this.count++
-        let data=this.pool.shift()
-        if(this.opt.callback&&typeof this.opt.callback=='function'){
-            this.opt.callback(data,this.done)
-        }else{
-            this.done()
-        }
-    }
 }
 
 jobs.prototype._done=function(){
@@ -36,7 +35,7 @@ jobs.prototype._done=function(){
     if(this.count===0 && this.pool.length===0){
         this.emit('drain')
     }else{
-        this._schedule()
+        this.emit('schedule')
     }
 }
 
